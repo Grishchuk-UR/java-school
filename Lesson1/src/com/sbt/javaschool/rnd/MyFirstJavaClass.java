42c76ea19917aefd11144aacd0dda6a3be28a64b
Fiast <ifast@rniirs.ru>
2019-10-25 14:02:36 +0300

    test data

--- Lesson1/src/com/sbt/javaschool/rnd/MyFirstJavaClass.java
+++ Lesson1/src/com/sbt/javaschool/rnd/MyFirstJavaClass.java
@@ -3,6 +3,10 @@ package com.sbt.javaschool.rnd;
 public class MyFirstJavaClass {
     public static void main(String[] args)
     {
+        MySecondJavaClass myClass = new MySecondJavaClass();
         System.out.println("Hello World");
+        System.out.println("  "+ myClass.ReturnStringData("Hello World"));
+
+
     }
 }
---
+++ Lesson1/src/com/sbt/javaschool/rnd/MySecondJavaClass.java
@@ -0,0 +1,8 @@
+package com.sbt.javaschool.rnd;
+
+public class MySecondJavaClass {
+    public String ReturnStringData(String _data)
+    {
+        return _data+" My String";
+    }
+}
