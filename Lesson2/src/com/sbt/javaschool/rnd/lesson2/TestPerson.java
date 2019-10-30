39537a5bd5e75f7bb251bde2ad7a5dbc54363e04
Fiast_o_O <Fiast1024@mail.ru>
2019-10-30 15:15:42 +0300

    home change

--- Lesson2/src/com/sbt/javaschool/rnd/lesson2/TestPerson.java
+++ Lesson2/src/com/sbt/javaschool/rnd/lesson2/TestPerson.java
@@ -2,13 +2,12 @@ package com.sbt.javaschool.rnd.lesson2;
 
 public class TestPerson {
     public static void main(String[] args) {
-        InterfacePerson person = new Person(true, "Sam", true);
-        InterfacePerson person1 = new Person(false, "Lile", false);
-        if (person.Marry((Person) person1)) {
-            System.out.println("Yes");
-        }
+        Person person = new Person(true, "Sam", 22);
+        Person person1 = new Person(false, "Lile", 26);
+        person.Marry( person1);
 
-        person1.Marry(new Person(true, "Don", false));
+
+        person1.Marry(new Person(true, "Don", 33));
         //person.spouse = new Person(false, "Lile", true);
 
 
